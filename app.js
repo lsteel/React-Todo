20db3ac66851a2fbec88da8752828ad9df859f22
Lorin Steel <lorinsteel@gmail.com>
2016-03-01 09:51:08 -0700

    Adds index.js, todos.js. Updates app.js

--- app.js
+++ app.js
@@ -2,9 +2,13 @@
 
 import config from 'config';
 import express from 'express';
+import api from './routes';
 
 const app = express();
+const PORT = config.port;
 
-app.listen(config.port, () => {
-  console.log(`Server listening on port $(PORT)`);
+app.use('/api', api);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
