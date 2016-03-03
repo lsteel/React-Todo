eb2f9dce9115649d1f19733e4865edd7c90c02c5
Lorin Steel <lorinsteel@gmail.com>
2016-03-03 09:48:19 -0700

    Updated app.js, added body-parser, updated index.js and todos.js routes.

---
+++ routes/todos.js
@@ -0,0 +1,39 @@
+'use strict';
+
+import express from 'express';
+
+const { Router } = express;
+const todosRouter = new Router();
+
+const todos = [];
+
+todosRouter.get('/', (req, res, next) => {
+  res.json(todos);
+});
+
+todosRouter.post('/', (req, res, next) => {
+  if (typeof req.body.name !== 'string') {
+    res.status(400).send('You must include a `name` field');
+  }
+  if (typeof req.body.completed !== 'boolean') {
+    req.body.completed = (req.body.completed === 'true') ? true : false;
+  }
+  let todo = {
+    name: req.body.name,
+    completed: req.body.completed
+  };
+  todos.push(todo);
+  res.json(todo);
+});
+
+todosRouter.get('/:id', (req, res, next) => {
+  let todo = todos[req.params.id];
+  if (!todo) {
+    return res.status(404).send('Todo not found with given id.');
+  }
+  else {
+    res.json(todo);
+  }
+})
+
+export default todosRouter;
