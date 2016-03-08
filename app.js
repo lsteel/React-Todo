86f1e0376480bffbb7d1253212b032fb5a791e57
Lorin Steel <lorinsteel@gmail.com>
2016-03-08 09:56:49 -0700

    NOW up to date with upstream

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-'use strict';
-
-import config from 'config';
-import express from 'express';
-import bodyParser from 'body-parser';
-import api from './routes';
-
-const app = express();
-const PORT = config.port;
-
-app.use(bodyParser.json());
-app.use('/api', api);
-
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
-=======
 'use strict'
 
 import config from 'config'
@@ -32,4 +14,3 @@ app.use('/api', api)
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
 })
->>>>>>> upstream/master
--- init.js
+++ init.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-'use strict';
-
-require('babel-core/register'); // This starts up the babel runtime parser
-require('babel-polyfill'); // This provides the extra es6 items like Promises and Array methods
-require('./app'); // Starts up the app be requiring our `index.js.
-=======
 'use strict'
 
 require('babel-core/register')
 require('babel-polyfill')
 require('./app')
->>>>>>> upstream/master
