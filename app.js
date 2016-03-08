83ab32c0a3073a0fae26daef919acc25481256ad
ksmithut <ksmith.ut@gmail.com>
2016-03-08 09:44:05 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict'
+
+import config from 'config'
+import express from 'express'
+import bodyParser from 'body-parser'
+import api from './routes'
+
+const app = express()
+const PORT = config.port
+
+app.use(bodyParser.json())
+app.use('/api', api)
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`)
+})
---
+++ routes/todos.js
@@ -0,0 +1,44 @@
+'use strict'
+
+import express from 'express'
+import Todo from '../lib/todos'
+
+const { Router } = express
+const todosRouter = new Router()
+
+todosRouter.route('/')
+  .get((req, res, next) => {
+    Todo.find((err, todos) => {
+      if (err) return next(err)
+      res.json(todos)
+    })
+  })
+  .post((req, res, next) => {
+    Todo.create(req.body, (err, todo) => {
+      if (err) return next(err)
+      res.status(201).json(todo)
+    })
+  })
+
+todosRouter.route('/:id')
+  .get((req, res, next) => {
+    Todo.findOne(req.params.id, (err, todo) => {
+      if (err) return next(err)
+      if (!todo) return res.sendStatus(404)
+      res.json(todo)
+    })
+  })
+  .put((req, res, next) => {
+    Todo.update(req.params.id, req.body, (err, todo) => {
+      if (err) return next(err)
+      res.json(todo)
+    })
+  })
+  .delete((req, res, next) => {
+    Todo.delete(req.params.id, (err) => {
+      if (err) return next(err)
+      res.sendStatus(204)
+    })
+  })
+
+export default todosRouter
