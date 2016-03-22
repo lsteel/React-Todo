3bc8964ec4dfaff437bd112fec743f993ad3d51f
lsteel <lorinsteel@gmail.com>
2016-03-22 09:49:15 -0600

    Now building using webpack.

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-'use strict'
+'use strict';
 
-import config from 'config'
-import express from 'express'
-import bodyParser from 'body-parser'
-import api from './routes'
+import path from 'path';
+import config from 'config';
+import express from 'express';
+import bodyParser from 'body-parser';
+import api from './routes';
 
-const app = express()
-const PORT = config.port
+const app = express();
+const PORT = config.port;
+const PUBLIC_DIR = path.join(__dirname, 'public');
 
-app.use(bodyParser.json())
-app.use('/api', api)
+app.use(bodyParser.json());
+app.use('/api', api);
+app.use(express.static(PUBLIC_DIR));
 
-app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`)
-})
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: './client/entry.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/, // Telling webpack to use files that match this pattern
+        loader: 'babel', // Uses the module `babel-loader`
+        exclude: /node_modules/, // Don't transpile modules in the `node_modules/` directory
+        include: __dirname, // Include all other files
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+};
