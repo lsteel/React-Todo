ed37af364e885af4d41c55eed81db5ee0533ad3b
ksmithut <ksmith.ut@gmail.com>
2016-04-05 08:25:50 -0600

    update with class

---
+++ client/root.js
@@ -0,0 +1,14 @@
+'use strict'
+
+import React from 'react'
+import { Provider } from 'react-redux'
+import configureStore from './store.js'
+import App from './components/App'
+
+export default (initialState) => {
+  return (
+    <Provider store={configureStore(initialState)}>
+      <App />
+    </Provider>
+  )
+}
---
+++ client/store.js
@@ -0,0 +1,38 @@
+'use strict'
+
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+
+// This is a stubbed reducer. We'll use a real one in a bit
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case 'TODO_CREATE':
+      return [
+        ...state,
+        action.payload
+      ]
+    case 'TODO_UPDATE':
+      return state.map((todo) => {
+        if (todo.id === action.payload.id) {
+          todo = Object.assign({}, todo, action.payload.partial)
+        }
+        return todo
+      })
+    case 'TODO_DELETE':
+      return state.filter((todo) => {
+        return todo.id !== action.payload
+      })
+    default:
+      return state
+  }
+}
+
+const devTools = typeof window === 'object'
+  && typeof window.devToolsExtension !== 'undefined'
+
+export default (initialState) => {
+  return createStore(reducer, initialState, compose(
+    applyMiddleware(thunk),
+    devTools ? window.devToolsExtension() : (f) => f
+  ))
+}
